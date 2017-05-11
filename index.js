4b46986c7c50350cbecd94c8b651a3e0055194dc
Josh Hawkins <jkhaccounts@gmail.com>
2017-05-11 00:36:29 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const Registry = require("winreg");
+const isAdmin = require("is-admin");
+
+isAdmin().then(admin => {
+  // "Note: For writing to this hive your program has to run with admin privileges" - winreg docs
+  if (!admin) console.warn("Not running as administrator!");
+
+  listCurrentKeys(items => items.map(item => console.log(item.key)));
+});
+
+function listCurrentKeys(callback) {
+  var regKey = Registry({
+    hive: Registry.HKCR,
+    key: "\\Directory\\Background\\shell"
+  });
+
+  regKey.keys((err, items) => {
+    if (err) console.error("ERROR: " + err);
+    else callback(items);
+  });
+}
